750e517bd6c3de164cd487a566ac779a14c90df0
Thomas Juranek <thomas@juranek.com>
2018-01-10 22:16:45 -0600

    Single Chart View

--- js/controllers/charts.js
+++ js/controllers/charts.js
@@ -1,16 +1,41 @@
 angular.module('MCM.charts', [])
 
-.controller('ChartsCtrl', function($scope, $http, UserServ, DataServ) {      
+.controller('ChartsCtrl', function($scope, $http, UserServ, DataServ) {     
+    // GET THE USER 
     $scope.user = UserServ.user;
 
+    // SET PAGE VALUES
+    $scope.displayType = 1;
+    $scope.chartOneCoin = "BTC";
+    $scope.chartTwoCoin = "ETH";
+ 
+    // GET THE DATA
     DataServ.getData()
     .then(function(returnData) {
         $scope.data = returnData.myData;
-
-        drawLineChart("BTC");
+        loadPage();
     });
 
-    function drawLineChart(cryptoSym) {
+    function loadPage() {
+        for (var a = 0; a < $scope.data.marketData.length; a++) {
+            if ($scope.data.marketData[a].symbol == $scope.chartOneCoin) {
+                $scope.chartOneData = $scope.data.marketData[a];
+            }
+        }
+
+        for (var a = 0; a < $scope.data.marketData.length; a++) {
+            if ($scope.data.marketData[a].symbol == $scope.chartTwoCoin) {
+                $scope.chartTwoData = $scope.data.marketData[a];
+            }
+        }
+
+        drawChartOne($scope.chartOneCoin);
+        drawChartTwo($scope.chartTwoCoin);
+    }
+
+
+
+    function drawChartOne(cryptoSym) {
         var url = "https://min-api.cryptocompare.com/data/histoday?fsym=" + cryptoSym + "&tsym=USD&limit=6";
 
         $http.get(url)
